0b0292040718b049a43eca848d50b208d4c85b45
KemsCh05 <chavarriamka@gmail.com>
2021-01-08 20:17:23 -0600

    Estructuras

--- ProyectoProgra2.c
+++ ProyectoProgra2.c
@@ -1,2 +1,24 @@
 #include <stdio.h>
-//Prueba
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct ninos Ninos;
+typedef struct Registros Registro_Ninos;
+
+struct ninos{
+    Ninos *siguiente;
+    int ID;
+    char nombre[60];
+    char usuario[40];
+    char correo[40];
+    char fecha_nacimiento[40];
+    int edad;
+    char necesidades_especiales[10][60];
+    int cant;
+};
+
+struct Registros
+{
+    Ninos *Inicio;
+    Ninos *Final;
+};
