9afed8c50f63c6052f31d2e5d0498c18a512d9e4
KemsCh05 <chavarriamka@gmail.com>
2021-01-08 23:14:21 -0600

    Modificar datos del niño registrado

--- ProyectoProgra2.c
+++ ProyectoProgra2.c
@@ -82,3 +82,222 @@ void Agregar_Nino(Registro_Ninos *Lista, int ID, char *nombre, char *usuario, ch
     }    
     printf("Se registro el nino exitosamente \n");
 }
+
+void modificar_datos_nino(Registro_Ninos *Lista, int ID, int seleccion){
+    Ninos *puntero = Lista->Inicio;
+    int validacion = 0;
+
+    // Ciclo para buscar en la lista de niños un niño que coincida con el ID ingresado que se modificará/eliminará
+    while (puntero != NULL)
+    {
+
+        // En caso de que se encuentre el niño que coincide con el ID ingresado
+        if (puntero->ID == ID){
+            validacion++;
+
+            // En caso de que se desee modificar los datos
+            if (seleccion == 1){
+                int seleccion2;
+                int a = 0;
+
+                // Este ciclo se repetirá hasta que el usuario ingrese un número de opción válido
+                while(a == 0){
+
+                    // Se consulta al usuario el dato que desea modificar
+                    printf("Ingrese 1 para modificar el ID del nino \nIngrese 2 para modificar el nombre del nino \nIngrese 3 para modificar el usuario del nino \nIngrese 4 para modificar el correo del nino \nIngrese 5 para modificar la fecha de nacimiento del nino \nIngrese 6 para modificar la edad del nino \nIngrese 7 para modificar las necesidades especiales del nino \nIngrese 8 para cancelar \n");
+                    scanf("%i", seleccion2);
+
+                    // En caso de que se desee modificar la identificación
+                    if (seleccion2 == 1)
+                    {
+                        int ID2;
+                        printf("Ingrese la nueva identificacion del nino(a): \n");
+                        scanf("%i", &ID2);
+                        puntero->ID = ID2;
+                        printf("Se modifico la identificacion del nino exitosamente \n");
+                        a++;
+                    }
+
+                    // En caso de que se desee modificar el nombre
+                    else if (seleccion2 == 2 && a == 0)
+                    {
+                        char nombre[60];
+                        printf("Ingrese el nuevo nombre del nino(a): \n");
+                        scanf("%s", &nombre);
+                        strcpy(puntero->nombre, nombre);
+                        printf("Se modifico el nombre del nino exitosamente \n");
+                        a++;
+                    }
+
+                    // En caso de que se desee modificar el nombre de usuario
+                    else if (seleccion2 == 3 && a == 0)
+                    {
+                        char usuario[40];
+                        printf("Ingrese el nuevo nombre de usuario del nino(a): \n");
+                        scanf("%s", &usuario);
+                        strcpy(puntero->usuario, usuario);
+                        printf("Se modifico el nombre de usuario del nino exitosamente \n");
+                        a++;
+                    }
+
+                    // En caso de que se desee modificar el correo
+                    else if (seleccion2 == 4 && a == 0)
+                    {
+                        char correo[40];
+                        printf("Ingrese el nuevo correo del nino(a): \n");
+                        scanf("%s", &correo);
+                        strcpy(puntero->correo, correo);
+                        printf("Se modifico el correo del nino exitosamente \n");
+                        a++;
+                    }
+
+                    // En caso de que se desee modificar la fecha de nacimiento
+                    else if (seleccion2 == 5 && a == 0)
+                    {
+                        char fecha_n[40];
+                        printf("Ingrese la nueva fecha de nacimiento del nino(a): \n");
+                        scanf("%s", &fecha_n);
+                        strcpy(puntero->fecha_nacimiento, fecha_n);
+                        printf("Se modifico la fecha de nacimiento del nino exitosamente \n");
+                        a++;
+                    }
+
+                    // En caso de que se desee modificar la edad
+                    else if (seleccion2 == 6 && a == 0)
+                    {
+                        int edad;
+                        printf("Ingrese la nueva edad del nino(a): \n");
+                        scanf("%i", &edad);
+                        puntero->edad = edad;
+                        printf("Se modifico la edad del nino exitosamente \n");
+                        a++;
+                    }
+
+                    // En caso de que se desee modificar las necesidades especiales
+                    else if (seleccion2 == 7 && a == 0)
+                    {
+                        int cant;
+                        printf("Ingrese la cantidad de necesidades especiales que desea agregar: \n");
+                        scanf("%i", &cant);
+                        int i;
+                        for ( i = 0; i < cant; i++)
+                        {
+                            char necesidad[60]; 
+                            printf("Ingrese la necesidad %i: \n", i+1);
+                            scanf("%s", &necesidad);
+                            strcpy(puntero->necesidades_especiales[i], necesidad);
+                        }
+                        printf("Se modifico las necesidades especiales del nino exitosamente \n");
+                        a++;
+                    }
+
+                    // En caso de que se desee cancelar
+                    else if (seleccion2 == 8 && a == 0)
+                    {
+                        printf("No se ha modificado ningun dato \n");
+                        a++;
+                    }
+
+                    // En caso de que se ingrese un dato inválido
+                    else
+                        printf("El numero de opcion ingresada no es valida \n");
+                } 
+            }
+
+            // En caso de que se desee eliminar un dato
+            if (seleccion == 2){
+                int a = 0;
+                int seleccion2;
+
+                // Este ciclo se repetirá hasta que el usuario ingrese un número de opción válido
+                while(a == 0){
+
+                    // Se consulta al usuario el dato que desea modificar
+                    printf("Ingrese 1 para eliminar el ID del nino \nIngrese 2 para eliminar el nombre del nino \nIngrese 3 para eliminar el usuario del nino \nIngrese 4 para eliminar el correo del nino \nIngrese 5 para eliminar la fecha de nacimiento del nino \nIngrese 6 para eliminar la edad del nino \nIngrese 7 para eliminar las necesidades especiales del nino \nIngrese 8 para cancelar \n");
+                    scanf("%i", seleccion2);
+
+                    //En caso de que se desee eliminar la identificación
+                    if (seleccion2 == 1)
+                    {
+                        puntero->ID = 0;
+                        printf("Se elimino la identificacion del nino exitosamente \n");
+                        a++;
+                    }
+
+                    //En caso de que se desee eliminar el nombre
+                    else if (seleccion2 == 2 && a == 0)
+                    {
+                        strcpy(puntero->nombre, "");
+                        printf("Se elimino el nombre del nino exitosamente \n");
+                        a++;
+                    }
+
+                    //En caso de que se desee eliminar el nombre de usuario
+                    else if (seleccion2 == 3 && a == 0)
+                    {
+                        strcpy(puntero->usuario, "");
+                        printf("Se elimino el nombre de usuario del nino exitosamente \n");
+                        a++;
+                    }
+
+                    //En caso de que se desee eliminar el correo
+                    else if (seleccion2 == 4 && a == 0)
+                    {
+                        strcpy(puntero->correo, "");
+                        printf("Se elimino el correo del nino exitosamente \n");
+                        a++;
+                    }
+
+                    //En caso de que se desee eliminar la fecha de nacimiento
+                    else if (seleccion2 == 5 && a == 0)
+                    {
+                        strcpy(puntero->fecha_nacimiento, "");
+                        printf("Se elimino la fecha de nacimiento del nino exitosamente \n");
+                        a++;
+                    }
+
+                    //En caso de que se desee eliminar la edad
+                    else if (seleccion2 == 6 && a == 0)
+                    {
+                        puntero->edad = 0;
+                        printf("Se elimino la edad del nino exitosamente \n");
+                        a++;
+                    }
+
+                    //En caso de que se desee eliminar las necesidades espaciales
+                    else if (seleccion2 == 7 && a == 0)
+                    {
+                        int cant;
+                        cant = puntero->cant;
+                        int i;
+                        for ( i = 0; i < cant; i++)
+                        {
+                            strcpy(puntero->necesidades_especiales[i], "");
+                        }
+                        printf("Se elimino las necesidades especiales del nino exitosamente \n");
+                        puntero->cant = 0;
+                        a++;
+                    }
+
+                    // En caso de que se desee cancelar
+                    else if (seleccion2 == 8 && a == 0)
+                    {
+                        printf("No se ha modificado ningun dato \n");
+                        a++;
+                    }
+
+                    // En caso de que se ingrese un dato inválido
+                    else
+                    {
+                        printf("El numero de opcion ingresada no es valida \n");
+                    } 
+                } 
+            }
+        }
+        puntero = puntero->siguiente;
+    }
+
+    // En caso de que no se encuentre un niño que coincida con el ID ingresado se notifica el error
+    if (validacion == 0)
+        printf("No se pudo modificar o eliminar la informacion pues la identificacion ingresada no corresponde a la de ningun nino registrado \n");
+}
