bab5c0d79c4a4cf0622f0b7b62be5693f8052e54
KemsCh05 <chavarriamka@gmail.com>
2021-01-08 21:34:58 -0600

    Crear ID del niño y agregarlo

--- ProyectoProgra2.c
+++ ProyectoProgra2.c
@@ -22,3 +22,63 @@ struct Registros
     Ninos *Inicio;
     Ninos *Final;
 };
+
+Ninos *CrearNino(int ID, char *nombre, char *usuario, char *correo, char *fecha_n, int edad, char necesidades[10][60], int cant){
+
+    //Se crea un nuevo nodo apartando el espacio en memoria
+    Ninos *NuevoNino = (Ninos *) malloc(sizeof(Ninos));
+    NuevoNino->siguiente = NULL;
+
+    //Agregamos los datos
+    NuevoNino->ID = ID;
+    strcpy(NuevoNino->nombre, nombre); 
+    strcpy(NuevoNino->usuario, usuario);
+    strcpy(NuevoNino->residencia, lugar_residencia);
+    strcpy(NuevoNino->correo, correo); 
+    strcpy(NuevoNino->fecha_nacimiento, fecha_n);
+    NuevoNino->edad = edad;
+    NuevoNino->cant = cant;
+    NuevoNino->comportamientos_malos = 0;
+    int i;
+    for (i = 0; i < cant; i++)
+    {
+        strcpy(NuevoNino->necesidades_especiales[i], necesidades[i]);
+    }
+
+    //Devolvemos el nodo creado
+    return NuevoNino; 
+}
+
+void Agregar_Nino(Registro_Ninos *Lista, int ID, char *nombre, char *usuario, char *correo, char *fecha_n, int edad, char necesidades[10][60], int cant){
+
+    //Se realiza un recorrido a la lista de niños para validar que no se repitan identificaciones
+    Ninos *puntero = Lista->Inicio;
+    printf("\n\n");
+    while (puntero != NULL)
+    {
+        if (puntero->ID == ID)
+        {
+            printf("Error, Ya existe un nino con esa identificacion. Ingrese otra identificacion y vuelva a intentarlo.\n");
+            return;
+        }
+        puntero = puntero->siguiente;
+    }
+
+    //Se crea el nuevo niño
+    Ninos *Nuevo = CrearNino(ID, nombre, usuario, correo, fecha_n, edad, necesidades, cant);
+
+    //En caso de que la lista este vacia 
+    if (Lista->Inicio == NULL)
+    {
+        Lista->Inicio = Nuevo;
+        Lista->Final = Nuevo;
+    }
+
+    //En caso de que la lista no este vacia
+    else
+    {
+        Lista->Final->siguiente = Nuevo;
+        Lista->Final = Nuevo;
+    }    
+    printf("Se registro el nino exitosamente \n");
+}
